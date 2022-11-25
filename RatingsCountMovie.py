9631e646688e39697a964eda77321817c9bf06b7
Ghliah_maher <49250490+ghaliahmaher@users.noreply.github.com>
2022-11-25 14:43:41 +0300

    Add files via upload

---
+++ RatingsCountMovie.py
@@ -0,0 +1,17 @@
+
+from mrjob.job import MRJob
+from mrjob.job import MRStep
+
+class RatingsCountMovie(MRJob):
+    # Define single mapreduce phase
+    def steps(self):
+        return [MRStep(mapper=self.mapper_get_ratings,reducer=self.reducer_count_ratings)]
+
+    def mapper_get_ratings(self, _, line):
+        (userID, movieID, rating, timestamp) = line.split('\t')
+        yield (movieID, rating),1
+
+    def reducer_count_ratings(self, key, values):
+        yield key, sum(values)
+if __name__ == '__main__':
+    RatingsCountMovie.run()
