a618be1352aabc8d80c3e9ec37c0b8212725b837
Ghliah_maher <49250490+ghaliahmaher@users.noreply.github.com>
2022-11-26 19:07:09 +0300

    Add files via upload

---
+++ Count_Family_account.py
@@ -0,0 +1,17 @@
+
+from mrjob.job import MRJob
+from mrjob.job import MRStep
+
+class Count_Family_account(MRJob):
+    # Define single mapreduce phase
+    def steps(self):
+        return [MRStep(mapper=self.mapper_get_ratings,reducer=self.reducer_count_ratings)]
+
+    def mapper_get_ratings(self, _, line):
+        (userID,program_name,duration_in_hour, program_class,genre, hd,original_name,commercial,family,score) = line.split('\t')
+        yield (userID,family), 1
+
+    def reducer_count_ratings(self, key, values):
+        yield key, sum(values)
+if __name__ == '__main__':
+    Count_Family_account.run()
